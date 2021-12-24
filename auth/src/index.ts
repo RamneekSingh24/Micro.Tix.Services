d2eae5d9ee9fbbb8c634dbf538f375c2059f0152
RamneekSingh24 <ramneeksingh2001@gmail.com>
2021-12-25 00:58:39 +0530

    auth service complete

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,17 +1,52 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
+
 import { currentUserRouter } from "./routes/current-user";
-import { signInRouter } from "./routes/singin";
-import { signOutRouter } from "./routes/signout";
-import { signUpRouter } from "./routes/signup";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
-app.listen(3000, () => {
-  console.log("Auth service running on port 4000..");
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
 });
+
+app.use(errorHandler);
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDb");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!!!!!!");
+  });
+};
+
+start();
--- auth/src/routes/singin.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.post("/api/users/signin", (req, res) => {
-  res.send("ola bb");
-});
-
-export { router as signInRouter };
